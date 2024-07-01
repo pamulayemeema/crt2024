a8795f3d4eb92e9207fb88fe1e97edf8647eea74
pamulayemeema <yemeemapamula@gmail.com>
2024-07-01 16:05:00 +0530

    Create read n strings into array and print in sorted order.java

---
+++ read n strings into array and print in sorted order.java
@@ -0,0 +1,44 @@
+
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        String s1[]=new String[n];
+        for(int i=0;i<n;i++)
+        {
+            s1[i]=s.next();
+        }
+        
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(s1[i].compareTo(s1[j])>0)
+                {
+                    String t=s1[i];
+                    s1[i]=s1[j];
+                    s1[j]=t;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s1[i]);
+        }
+       
+    }
+}
+
+
+
+4
+df
+uyt
+hbv
+uytr
+df
+hbv
+uyt
+uytr
