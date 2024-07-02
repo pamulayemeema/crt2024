297c5484addb62c3e26766e184cb56d376b85dac
pamulayemeema <yemeemapamula@gmail.com>
2024-07-02 16:06:57 +0530

    Create read string and print word by word.java

---
+++ read string and print word by word.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+        String res[]=s1.split(" ");
+        for(int i=0;i<res.length;i++)
+        System.out.println((res[i]));
+        
+    }
+}
+
+
+java is simple language
+java
+is
+simple
+language
