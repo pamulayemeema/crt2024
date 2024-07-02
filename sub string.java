4bc341afaa2b5063ffab2fa2a303c35ed83ebaf9
pamulayemeema <yemeemapamula@gmail.com>
2024-07-02 15:36:22 +0530

    Create sub string.java

---
+++ sub string.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+        String s2=s.nextLine();
+          if(s1.contains(s2))
+              System.out.println("Sub String");
+              else
+              System.out.println("not Sub String");
+        
+    }
+}
+
+
+wzrextcyvu
+wzr
+Sub String
