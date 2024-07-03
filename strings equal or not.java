0ac804f94ee754cb4b6686c71fa59465e79e32e1
pamulayemeema <yemeemapamula@gmail.com>
2024-07-03 23:03:46 +0530

    Create strings equal or not.java

---
+++ strings equal or not.java
@@ -0,0 +1,19 @@
+equals() :
+-----------------
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+       Scanner s=new Scanner(System.in);
+       String s1=s.next();
+       String s2=s.next();
+       if(s1.equals(s2))
+        System.out.println("Strings are same");
+       else
+        System.out.println("Strings are not same");
+     
+    }
+}
+
+wer
+wer
+Strings are same
